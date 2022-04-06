22ecdae1f12b4d080db25e3eab7cadec328d2b8f
efleung2 <efleung2@illinois.edu>
2022-04-06 16:55:52 -0500

    Finish basic game logic implementation

--- crabgame_ai/bombtag.py
+++ crabgame_ai/bombtag.py
@@ -8,17 +8,17 @@ from typing import Sequence
 import gym
 import numpy as np
 
-game_map = np.array([[0,0,0,0,1,1,1,1],
-                [0,0,0,0,1,1,1,1],
-                [0,0,0,0,0,0,2,1],
-                [0,0,0,0,0,0,0,1],
-                [0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0]])
+# example map
+game_map = np.array([[0, 0, 0, 0, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 1, 1, 1, 1],
+                     [0, 0, 0, 0, 0, 0, 2, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 1],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0]])
 # seeker, hider
-start_locs = [(7,7,3),(0,0,1)]
-
+start_locs = [(7, 7, 3), (0, 0, 1)]
 
 
 class BombTagActions(IntEnum):
@@ -45,8 +45,9 @@ class Direction(IntEnum):
     SOUTH = 2
     WEST = 3
 
+
 class CellType(IntEnum):
-    """A class that defines the available directions in the game."""
+    """A class that defines the possible cell types in the game."""
     GROUND = 0
     PLATFORM = 1
     STAIRCASE = 2
@@ -73,7 +74,7 @@ class Player:
         self.x = init_location[0]
         self.y = init_location[1]
         self.direction = init_location[2]
-        self.init_loc = init_location # save initial location for reset
+        self.init_loc = init_location  # save initial location for reset
 
     def reset_player(self):
         """
@@ -84,34 +85,11 @@ class Player:
         self.y = self.init_loc[1]
         self.direction = self.init_loc[2]
 
-    # def move_forward(self):
-    #     match self.direction:
-    #         case 0:
-    #             if self.y > 0:
-    #                 self.y -= 1
-    #         case 1:
-    #             if self.x < width: # how are we defining map width?
-    #                 self.x += 1
-    #         case 2:
-    #             if self.y < height:
-    #                 self.y += 1
-    #         case 3:
-    #             if self.x > 0:
-    #                 self.x -= 1
-
     def turn_left(self):
         self.direction = (self.direction - 1) % 4
 
     def turn_right(self):
-        self.direction = (self.direction + 1) % 4 # int or direction enum? does it matter?
-
-    def hand_bomb(self):
-        if self.has_bomb: # also check if cell facing contains opponent
-            self.has_bomb = 0
-            return True # let step know bomb successfully handed over
-        else:
-            return False
-
+        self.direction = (self.direction + 1) % 4
 
 
 class BombTagObservation:
@@ -121,6 +99,7 @@ class BombTagObservation:
     - The current position of the seeker and hider
     - The remaining explode time of the bomb
     """
+
     def __init__(self, cur_env: np.ndarray, time: int):
         self.remaining_time = time
         self.seeker_pos = np.where(cur_env == BombTagPlayerTypes.SEEKER)
@@ -132,13 +111,13 @@ def forward_cell(player: Player):
     y = player.y
     match player.direction:
         case 0:
-                y -= 1
+            y -= 1
         case 1:
-                x += 1
+            x += 1
         case 2:
-                y += 1
+            y += 1
         case 3:
-                x -= 1
+            x -= 1
     return x, y
 
 
@@ -147,19 +126,27 @@ class BombTagEnv(gym.Env):
     the OpenAI Gym interface."""
 
     def __init__(self, env_map: np.ndarray, init_locations: list, explode_time: int = 100):
-        self.init_explode_time = explode_time
+        self.init_explode_time = explode_time  # save for reset
         self.explode_time = explode_time
-        self.init_env = env_map
-        self.grid = env_map
+        self.init_env = env_map  # basic environment map w/o player locations
+        self.cur_env = env_map  # game map w/ player locations
         self.map_height, self.map_width = env_map.shape
         # self.players = []
         # for i in range(2):
         #     player = Player(BombTagPlayerTypes(i), i, init_locations[i])
         #     self.players.append(player)
+
+        # player 1: id=1, type=Seeker
         self.player1 = Player(BombTagPlayerTypes.SEEKER, 0, init_locations[0])
-        self.grid[init_locations[0][0], init_locations[0][1]] = CellType.SEEKER
+        # add player 1's initial location to the current game map
+        self.cur_env[init_locations[0][0], init_locations[0][1]] = CellType.SEEKER
+
+        # player 2: id=1, type=Hider
         self.player2 = Player(BombTagPlayerTypes.HIDER, 1, init_locations[1])
-        self.grid[init_locations[1][0], init_locations[1][1]] = CellType.HIDER
+        # add player 2's initial location to the current game map
+        self.cur_env[init_locations[1][0], init_locations[1][1]] = CellType.HIDER
+
+        # create list of players
         self.players = [self.player1, self.player2]
 
     def reset(self):
@@ -187,50 +174,74 @@ class BombTagEnv(gym.Env):
 
         TODO: implement this method.
         """
-        rewards = []
+        rewards = np.zeros(len(self.players))
         done = self.explode_time <= 0
+
         if done:
-            return BombTagObservation(self.grid, self.explode_time), rewards, done
+            for p in self.players:
+                if not p.has_bomb:
+                    # award player for not exploding
+                    rewards[p.player_id] = 1
+                elif p.has_bomb:
+                    # penalize player for exploding
+                    rewards[p.player_id] = -1
+            return BombTagObservation(self.cur_env, self.explode_time), rewards, done
 
         # action index in array corresponds to each player id
         for i in range(len(actions)):
-        # for action in actions:
             player = self.players[i]
             x = player.x
             y = player.y
-            direction = player.direction
+            # coordinates of forward cell
             f_x, f_y = forward_cell(player)
+
             match actions[i]:
                 case BombTagActions.MOVE_FORWARD:
                     # cant if facing wall
-                    # cant if ground and facing platform
-                    if 0 < f_x < self.map_width and 0 < f_y < self.map_height \
-                            and not(self.grid[x, y] == 0 and self.grid[f_x, f_y] == 1):
-                        # overlapping players?
-                        player.x = f_x
-                        player.y = f_y
-                        self.grid[x, y] = self.init_env[x, y]
-                        self.grid[f_x, f_y] = CellType(player.player_type + 3)
+                    if 0 < f_x < self.map_width and 0 < f_y < self.map_height:
+                        # other than if facing stairs, must be same tile type
+                        if self.init_env[x, y] == self.cur_env[f_x, f_y] or self.cur_env[f_x, f_y] == CellType.STAIRCASE:
+                            player.x = f_x
+                            player.y = f_y
+                            self.cur_env[x, y] = self.init_env[x, y]
+                            self.cur_env[f_x, f_y] = CellType(player.player_type + 3)
+                        # player dies if fall off the platform, game end?
+                        if self.init_env[x, y] == CellType.PLATFORM and self.init_env[f_x, f_y] == CellType.GROUND:
+                            rewards[player.player_id] = -1
+                            done = True
+                            return BombTagObservation(self.cur_env, self.explode_time), rewards, done
                 case BombTagActions.TURN_LEFT:
                     player.turn_left()
                 case BombTagActions.TURN_RIGHT:
                     player.turn_right()
                 case BombTagActions.STAY_STILL:
                     # do nothing
+                    continue
                 case BombTagActions.JUMP:
-                    # if facing platform, move forward
+                    # if facing opposite level, move forward
+                    if 0 < f_x < self.map_width and 0 < f_y < self.map_height:
+                        if (self.init_env[x, y] == CellType.PLATFORM and self.init_env[f_x, f_y] == CellType.GROUND) \
+                                or (self.init_env[x, y] == CellType.GROUND and self.init_env[f_x, f_y] == CellType.PLATFORM):
+                            player.x = f_x
+                            player.y = f_y
+                            self.cur_env[x, y] = self.init_env[x, y]
+                            self.cur_env[f_x, f_y] = CellType(player.player_type + 3)
                 case BombTagActions.HAND_BOMB:
                     if player.has_bomb and \
                             (0 < f_x < self.map_width and 0 < f_y < self.map_height) \
-                            and self.grid[f_x, f_y] == CellType.HIDER:
+                            and self.cur_env[f_x, f_y] == CellType.HIDER:
                         player.has_bomb = False
-                        # set receiving player has_bomb = True, but need to know handed bomb to who
-                    # player.hand_bomb()
+                        player.player_type = BombTagPlayerTypes.HIDER
+                        # set receiving player has_bomb = True, assume other player is hider, obtains bomb
+                        self.players[1 - player.player_id].has_bomb = True
+                        self.players[1 - player.player_id].player_type = BombTagPlayerTypes.SEEKER
                 case _:
                     print('unknown action')
+
+        # decrease bomb time
         self.explode_time -= 1
 
-        return BombTagObservation(self.grid, self.explode_time), rewards, done
+        return BombTagObservation(self.cur_env, self.explode_time), rewards, done
 
     def render(self, mode='human'):
         """Render the environment. (use matplotlib.pyplot or cv2)
